300fb3d6c35cca63018a43dd3c5d7ed34ee63980
wsocia <wsocia@gmail.com>
2020-05-19 15:31:33 -0700

    Attempt to improve stability of Window handle

--- rename.py
+++ rename.py
@@ -1,14 +1,17 @@
 import os
 import webbrowser
 from tkinter import filedialog,simpledialog
+import tkinter
 import win32gui
 
+gui = tkinter.Tk()
+gui.title("Rename")
 fileDir = filedialog.askdirectory()
-handle = win32gui.FindWindow(0, "tk")
 i = 0
 
 for filename in os.listdir(fileDir):
     webbrowser.open_new(fileDir + "/" + filename)
+    handle = win32gui.FindWindow(None, "Rename")
     win32gui.SetForegroundWindow(handle)
     newName = simpledialog.askstring("Contract Rename Tool","Enter contract number (Enter 0 to skip): ")
     if newName == "0":
