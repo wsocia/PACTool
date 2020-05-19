fe8a9f6f46f3c76121b83ef49869c3a2cf957b96
wsocia <wsocia@gmail.com>
2020-05-19 13:03:22 -0700

    Improved handling of junk files

--- rename.py
+++ rename.py
@@ -5,12 +5,14 @@ import win32gui
 
 fileDir = filedialog.askdirectory()
 handle = win32gui.FindWindow(0, "tk")
+i = 0
 
 for filename in os.listdir(fileDir):
     webbrowser.open_new(fileDir + "/" + filename)
     win32gui.SetForegroundWindow(handle)
-    newName = simpledialog.askstring("Contract Rename Tool","Enter contract number (Enter 0 to delete): ")
+    newName = simpledialog.askstring("Contract Rename Tool","Enter contract number (Enter 0 to skip): ")
     if newName == "0":
-        os.remove(fileDir + "/" + filename)
+        os.rename(fileDir + "/" + filename,fileDir + "/0-junkfile" + str(i) + ".pdf")
+        i+=1
     else:
-        os.rename(fileDir + "/" + filename,fileDir + "/" + newName + ".pdf")
+        os.rename(fileDir + "/" + filename,fileDir + "/Contract}U}" + newName + "}U}OT}I}Scanned_copy.pdf")
