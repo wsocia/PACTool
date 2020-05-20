3e726262440baa004fcca30782ec875940759c4a
wsocia <wsocia@gmail.com>
2020-05-20 11:48:03 -0700

    Improved stability

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PACTool
 Tools for assisting with tasks at Partners Alliance Corporation
+
+TODO:
+
+Comment code lol
+Figure out deleting of junk instead of just renaming
+combine scripts into single user friendly GUI
+Get pyinstaller to build a portable exe for plebs (figure out why it won't include pywin32)
+add FTP function to auto upload completed files (need FTP credentials from company.{This may never get added...})
--- rename.py
+++ rename.py
@@ -1,16 +1,22 @@
 import os
 import webbrowser
 from tkinter import filedialog,simpledialog
-import win32gui
+import tkinter
+import win32gui, win32com.client
 import time
 
+gui = tkinter.Tk()
+gui.title("Rename")
 fileDir = filedialog.askdirectory()
-hwnd = win32gui.FindWindow(None, "tk")
+hwnd = win32gui.FindWindow(0,"Rename")
+print(hwnd)
 i = 0
 
 for filename in os.listdir(fileDir):
     webbrowser.open_new(fileDir + "/" + filename)
-    time.sleep(.5)
+    time.sleep(1)
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shell.SendKeys('%')
     win32gui.SetForegroundWindow(hwnd)
     newName = simpledialog.askstring("Rename","Enter contract number (Enter 0 to skip): ")
     if newName == "0":
