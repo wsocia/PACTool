f59667591e7a9197520a294d51d66ad8c40d8aa3
Wesley Socia <wsocia@cpsi.usa.gap>
2020-08-04 13:03:20 -0700

    Added file count to titlebar

--- rename.py
+++ rename.py
@@ -9,8 +9,9 @@ gui = tkinter.Tk()
 gui.title("Rename")
 fileDir = filedialog.askdirectory()
 hwnd = win32gui.FindWindow(0,"Rename")
-print(hwnd)
+print(len(os.listdir(fileDir)))
 i = 0
+c = 1
 
 for filename in os.listdir(fileDir):
     webbrowser.open_new(fileDir + "/" + filename)
@@ -18,7 +19,9 @@ for filename in os.listdir(fileDir):
     shell = win32com.client.Dispatch("WScript.Shell")
     shell.SendKeys('%')
     win32gui.SetForegroundWindow(hwnd)
-    newName = simpledialog.askstring("Rename","Enter contract number (Enter 0 to skip): ")
+    title = str(c) + "/" + str(len(os.listdir(fileDir)))
+    newName = simpledialog.askstring(title,"Enter contract number (Enter 0 to skip): ")
+    c+=1
     if newName == "0":
         os.rename(fileDir + "/" + filename,fileDir + "/0-junkfile" + str(i) + ".pdf")
         i+=1
