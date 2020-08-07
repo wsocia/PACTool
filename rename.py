c63900d7879827896913b382385d2c47ae8a7123
Wesley Socia <wsocia@cpsi.usa.gap>
2020-08-07 14:18:34 -0700

    minor update

--- rename.py
+++ rename.py
@@ -1,26 +1,19 @@
-import os
-import webbrowser
-from tkinter import filedialog,simpledialog
-import tkinter
-import win32gui, win32com.client
-import time
+import os, webbrowser, tkinter, win32gui, win32com
 
 gui = tkinter.Tk()
 gui.title("Rename")
-fileDir = filedialog.askdirectory()
+fileDir = tkinter.filedialog.askdirectory()
 hwnd = win32gui.FindWindow(0,"Rename")
-print(len(os.listdir(fileDir)))
 i = 0
 c = 1
 
 for filename in os.listdir(fileDir):
     webbrowser.open_new(fileDir + "/" + filename)
-    time.sleep(1)
     shell = win32com.client.Dispatch("WScript.Shell")
     shell.SendKeys('%')
     win32gui.SetForegroundWindow(hwnd)
     title = str(c) + "/" + str(len(os.listdir(fileDir)))
-    newName = simpledialog.askstring(title,"Enter contract number (Enter 0 to skip): ")
+    newName = tkinter.simpledialog.askstring(title,"Enter contract number (Enter 0 to skip): ")
     c+=1
     if newName == "0":
         os.rename(fileDir + "/" + filename,fileDir + "/0-junkfile" + str(i) + ".pdf")
